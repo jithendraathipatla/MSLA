eab15732c72e1dd5dbaf1d9fafcad4d232d319d9
jithendraathipatla <athipatala.jithendra@gmail.com>
2022-02-01 12:01:16 +0530

    building the things

--- src/components/data.jsx
+++ src/components/data.jsx
@@ -37,6 +37,12 @@ const Data = [
     buttonURL2_1_v: "https://xc-s-zw00443.xc.abb.com:8443/configuratorportal/controller?mailaddress=name.surname@hitachienergy.com&amp;EndUserCustomerPostal=SA&amp;productgroupid=3026&amp;productid=ELK_04_145_170_ALL&amp;ActivityLocationId=9AAE315107&amp;EndUserCustomerBillTo=CN&amp;readwrite=70",
     country: "CH",
   },
+  {
+    username: "jency.rb@in.abb.com",
+    buttonURL2_0_v: "https://xc-s-zw00443.xc.abb.com:8443/configuratorportal/controller?mailaddress=name.surname@hitachienergy.com&amp;EndUserCustomerPostal=SA&amp;productgroupid=3026&amp;productid=ELK_04_145_170_ALL&amp;version=2.0.0&amp;ActivityLocationId=9AAE315107&amp;EndUserCustomerBillTo=CN&amp;readwrite=70",
+    buttonURL2_1_v: "https://xc-s-zw00443.xc.abb.com:8443/configuratorportal/controller?mailaddress=name.surname@hitachienergy.com&amp;EndUserCustomerPostal=SA&amp;productgroupid=3026&amp;productid=ELK_04_145_170_ALL&amp;ActivityLocationId=9AAE315107&amp;EndUserCustomerBillTo=CN&amp;readwrite=70",
+    country: "CH",
+  },
 ];
 
 export default Data;
